9e38f06d6b7c3af7082e4d10cd3e17c1db3bb5e3
Hamlet450 <hjb5521@psu.edu>
2024-12-12 11:16:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Engine engine1 = new Engine(599, "Inline-4", 120);
+        Engine engine2 = new Engine(998, "V-Twin", 200);
+        Engine engine3 = new Engine(1103, "V4", 215);
 
-        Motorcycle moto1 = new Motorcycle("Honda", "867G77", 2022, 599, Color.RED);
-        Motorcycle moto2 = new Motorcycle("Yamaha", "734DA7", 2023, 998, Color.BlUE);
-        Motorcycle moto3 = new Motorcycle("Ducati", "800GIP", 2021, 1103, Color.YELLOW);
+        Owner owner1 = new Owner("John Doe", "12345ABC", "555-1234");
+        Owner owner2 = new Owner("Jane Smith", "67890XYZ", "555-5678");
+        Owner owner3 = new Owner("Mike Johnson", "11223DEF", "555-9012");
 
+        Registration reg1 = new Registration("ABC123", "CA", "12/2024");
+        Registration reg2 = new Registration("XYZ789", "NY", "06/2025");
+        Registration reg3 = new Registration("LMN456", "TX", "08/2026");
 
-        System.out.println("Motorcycle 1: " + moto1.getMake() + " " + moto1.getVin() + ", Year: " + moto1.getYear() + ", Engine: " + moto1.getHorsepower() + "cc, Color: " + moto1.getColor());
-        System.out.println("Motorcycle 2: " + moto2.getMake() + " " + moto2.getVin() + ", Year: " + moto2.getYear() + ", Engine: " + moto2.getHorsepower() + "cc, Color: " + moto2.getColor());
-        System.out.println("Motorcycle 3: " + moto3.getMake() + " " + moto3.getVin() + ", Year: " + moto3.getYear() + ", Engine: " + moto3.getHorsepower() + "cc, Color: " + moto3.getColor());
+        Motorcycle moto1 = new Motorcycle("Honda", "867G77", 2022, Color.RED, engine1, owner1, reg1);
+        Motorcycle moto2 = new Motorcycle("Yamaha", "734DA7", 2023, Color.BLUE, engine2, owner2, reg2);
+        Motorcycle moto3 = new Motorcycle("Ducati", "800GIP", 2021, Color.YELLOW, engine3, owner3, reg3);
+
+        System.out.println("Motorcycle 1:\n" + moto1);
+        System.out.println("Motorcycle 2:\n" + moto2);
+        System.out.println("Motorcycle 3:\n" + moto3);
     }
 }
